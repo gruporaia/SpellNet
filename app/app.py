d6fb614d237be87bc53f75fe26e33c70c8e4aa20
Daniel <danielcd3099@gmail.com>
2025-04-17 09:32:56 -0300

    added augmented mobilenet

--- app/app.py
+++ app/app.py
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.WARNING)
 callback_results = queue.Queue()
 green = "#33FF70"
 red = "#FF5733"
-model_path = './model/mobilenet_v2.keras'
+model_path = './model/mobilenet_aug.keras'
 
 st.set_page_config(page_title="SingLink", layout="centered")
 
